32a99f897aee21d11c4248cbcd4a9baceb8c36fc
brandongranucci <brandongranucci00@gmail.com>
2022-09-17 21:53:55 -0400

    first commit

---
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.flixster
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+private const val TAG = "MovieAdapter"
+class MovieAdapter(private val context: Context, private val movies: List<Movie>)
+    : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+    // Expensive Operation: Create a view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.i(TAG, "onCreateViewHolder")
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    //  Cheap: Simply bind data to an existing viewholder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i(TAG, "onBindViewHolder position $position")
+        val movie = movies[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount() = movies.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
+        private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
+        private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        fun bind(movie: Movie) {
+            tvTitle.text = movie.title
+            tvOverview.text = movie.overview
+            Glide.with(context)
+                .load(movie.posterImageUrl)
+                .placeholder(R.drawable.ic_loading_foreground)
+                .into(ivPoster)
+        }
+    }
+}
